1d0f5a30767b9a5124c90e84ce29eff0a848d20e
Bhavya Rathore <bhavyarathore575@gmail.com>
2024-09-01 13:37:05 +0530

    first commit

---
+++ Projects/Spotify/script.js
@@ -0,0 +1,39 @@
+console.log("Lets Write JS");
+
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:3000/Projects/spotify/songs/");
+  let response = await a.text();
+  // console.log(response)
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
+async function main() {
+  let songs = await getsongs();
+  console.log(songs);
+
+   let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + song;
+    }
+
+  // playing the first song
+  var audio = new Audio(songs[0]);
+  // audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  });
+}
+main();
